c92f3fac1adc14016a2a2476a51b938601662324
Riccardo <riccardocppzz93@gmail.com>
2015-10-31 21:18:48 +0100

    added sum with wait

---
+++ snippets/sum_with_wait.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/wait.h>
+
+#define MAX_VALUE 5
+
+int main(int argc, char ** argv)
+{
+    int i, n, val, status;
+    pid_t pids[MAX_VALUE];
+    int sum = 0;
+
+    n = atoi (argv[1]);
+
+    if (argc != 2) {
+        fprintf(stderr, "Wrong number of arguments\n");
+        exit(1);
+    }
+
+    if (n < 1) {
+        fprintf(stderr, "Number must be larger than 0\n");
+        exit(1);
+    }
+
+    if (n > MAX_VALUE) {
+        fprintf(stderr, "Number must be larger than %d\n", MAX_VALUE);
+        exit(1);
+    }
+
+
+    for (i = 1; i <= n; i++) {
+        pids[i-1] = fork();
+
+        if (pids[i-1] == 0) {
+            // child process
+            val = (int) pow(2, i);
+            exit(val);
+        } else {
+            // parent process
+            waitpid(pids[i-1], &status, 0);
+            sum+=(status>>8);
+        }
+
+    }
+    printf("The sum is %d\n", sum);
+
+
+    return 0;
+}
